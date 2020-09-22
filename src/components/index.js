a624abaaca4373eecaff4c8de09ac1fc181d7a9f
alan-wu <alan.wu@auckland.ac.nz>
2020-09-22 15:57:45 +1200

    Export is now named. Version update to 0.1.13.

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from "vue";
 import TooltipVuer from "./TooltipVuer.vue";
 
-const Components = {
-  TooltipVuer  
+export { 
+  TooltipVuer 
 };
-  
-Object.keys(Components).forEach(name => {
-  Vue.component(name, Components[name]);
-});
-  
-export default Components;
-  
